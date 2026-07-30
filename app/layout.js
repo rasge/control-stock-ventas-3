feb6ce90837c5feb548792f6457c303ae63b8beb
Raul <raul@control-stock.dev>
2026-07-29 21:22:45 -0300

    feat: agregar estilos y base de datos

---
+++ app/layout.js
@@ -0,0 +1,14 @@
+﻿export const metadata = {
+  title: "Control de Stock y Ventas",
+  description: "App para gestionar stock y registrar ventas",
+};
+
+import "./globals.css";
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="es">
+      <body>{children}</body>
+    </html>
+  );
+}
